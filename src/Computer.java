f13ff7e38ce3c3f6767125d2c81d7bd1efce40ce
Elias Zarate <Elias Zarate@Elias-Rig>
2024-04-19 16:41:35 -0700

    Fixed Commit

--- src/Computer.java
+++ src/Computer.java
@@ -2,11 +2,7 @@ import java.util.ArrayList;
 
 public class Computer {
     private ArrayList<Part> computerParts;
-<<<<<<< HEAD
-    //change
-=======
     // is
->>>>>>> branch 'main' of https://github.com/hiwhatsupguys/BuildaComputerCISC.git
     public Computer() {
     
     }
