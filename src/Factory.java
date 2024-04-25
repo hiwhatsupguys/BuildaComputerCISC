cc263cf37472feba1ca15967917dc043ffd0206c
Elias Zarate <Elias Zarate@Elias-Rig>
2024-04-24 17:15:14 -0700

    Added a Factory class to automate the creation of Part Objects

---
+++ src/Factory.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Factory
+{
+	public static List<Part> readPartsFromFile(String fileName)
+	{
+		try
+		{
+			//open the file
+			// use the scanner.useDelimiter method
+		}
+		catch()
+		{
+			// in the event something goes wrong
+		}
+		finally
+		{
+			//close the file
+		}
+		
+	}
+	
+	public static void savePartsToFile(List<Part> partsList)
+	{
+		try
+		{
+			// open the file...
+			
+		}
+		catch()
+		{
+			// in the event something goes wrong
+		}
+		finally
+		{
+			// close the file
+		}
+	}
+
+}
