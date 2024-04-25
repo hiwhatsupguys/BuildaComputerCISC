7bac911c0633725f8399ca462bb277d2837ffd3e
Elias Zarate <Elias Zarate@Elias-Rig>
2024-04-24 19:02:01 -0700

    Began Implementing try block for readPartsFromFile method in Factory

--- src/Factory.java
+++ src/Factory.java
@@ -1,23 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Factory
 {
-	public static List<Part> readPartsFromFile(String fileName)
+	public static List<Part> readPartsFromFile(String fileName) 
 	{
+		List<Part> parts = null;
+		File file;
+		Scanner sc;
+		
 		try
 		{
 			//open the file
+			file = new File(fileName);
+			sc = new Scanner(file);
+			
+			switch(sc.nextLine())
+			{
+				case 1:
+			}
+			
+			
 			// use the scanner.useDelimiter method
 		}
-		catch()
+		catch(FileNotFoundException e)
 		{
 			// in the event something goes wrong
+			System.out.println(e);
 		}
 		finally
 		{
 			//close the file
+			
 		}
 		
+		return part;
 	}
 	
 	public static void savePartsToFile(List<Part> partsList)
